e78be1dcaf172039ebb8e42a55304c2d2f9d2644
Halit2020 <halit-altun2018@yandex.com>
2020-11-27 21:50:14 +0100

    sepete ekllendi

---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1.islem");
+    }
+    public static void yeniMetod()
+    {
+        System.out.println("yeni metodum hazir");
+        System.out.println("sepete ekle kisimlari buralar");
+    }
+}
